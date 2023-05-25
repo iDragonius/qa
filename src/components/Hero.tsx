a2a827a968ba6251b8c5d4d9d2f0c7c596ffe694
iDragonius <rustamovruslan1205@gmail.com>
2023-05-25 23:08:26 +0400

    feat: test1

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,31 +4,29 @@ import React, { FC } from "react";
 import useTranslation from "next-translate/useTranslation";
 
 const Hero = ({}) => {
-  const {t} = useTranslation('common')
+  const { t } = useTranslation("common");
   const { data: session } = useSession();
   return (
     <div className="hero  mb-5 rounded-xl bg-base-200 py-10">
       <div className="hero-content text-center">
         <div className="max-w-md">
           <h1 className="text-5xl font-bold">
-            {t('welcome')}{" "}
+            <p>{t("welcome")} </p>
             <span className="bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-transparent">
               Answered
             </span>
           </h1>
-          <p className="py-6">
-            {t('description')}
-          </p>
+          <p className="py-6">{t("description")}</p>
           {session ? (
             <Link href={"/questions/ask"} className="btn-primary btn">
-              {t('ask')}
+              {t("ask")}
             </Link>
           ) : (
             <button
               className="btn-primary btn"
               onClick={async () => await signIn()}
             >
-              {t('auth')}
+              {t("auth")}
             </button>
           )}
         </div>
